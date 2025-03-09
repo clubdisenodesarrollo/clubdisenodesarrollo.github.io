9d41fcb53a76f892f8c095c3b642a8474ce425ad
clubdisenodesarrollo <161260649+clubdisenodesarrollo@users.noreply.github.com>
2025-03-08 21:37:34 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -14,7 +14,6 @@ window.addEventListener('scroll', function () {
 
 
 
-
 // Array de cartas
 const cartas = [
   {
@@ -59,9 +58,162 @@ const cartas = [
     tag: "proyectos", // Etiqueta para filtrar
     enlace: "/Proyectos/cartelera/index.html", // Enlace a la página del proyecto
   },
+  {
+    titulo: "Particulas - Imágenes",
+    descripcion: "",
+    imagen: "/Proyectos/ParticulasTerminado/fondo.png",
+    tag: "proyectos", // Etiqueta para filtrar
+    enlace: "/Proyectos/ParticulasTerminado/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Avatar, El último maestro del aire",
+    descripcion: "",
+    imagen: "/miniProyectos/avatar/fondo.png",
+    tag: "otros", // Etiqueta para filtrar
+    enlace: "/Proyectos/avatar/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Carrusel de Imágenes",
+    descripcion: "",
+    imagen: "/miniProyectos/carrucel3D/fondo.png",
+    tag: "otros", // Etiqueta para filtrar
+    enlace: "/miniProyectos/carrucel3D/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación de Circulos",
+    descripcion: "",
+    imagen: "/miniProyectos/circulo/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/circulo/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación de Cuadrados",
+    descripcion: "",
+    imagen: "/miniProyectos/cuadrados/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/cuadrados/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Efecto luces",
+    descripcion: "",
+    imagen: "/miniProyectos/efectoLuz/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/efectoLuz/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Efecto Olas",
+    descripcion: "",
+    imagen: "/miniProyectos/efectoOlas/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/efectoOlas/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Galería de Imágenes",
+    descripcion: "",
+    imagen: "/miniProyectos/galeriaEA/fondo.png",
+    tag: "otros", // Etiqueta para filtrar
+    enlace: "/miniProyectos/galeriaEA/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Atrapar el objetivo - Juego",
+    descripcion: "",
+    imagen: "/miniProyectos/juegoAtraparObjetivo/fondo.png",
+    tag: "juegos", // Etiqueta para filtrar
+    enlace: "/miniProyectos/juegoAtraparObjetivo/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Buscar los pares - Juego",
+    descripcion: "",
+    imagen: "/miniProyectos/juegoBusquedaPares/fondo.png",
+    tag: "juegos", // Etiqueta para filtrar
+    enlace: "/miniProyectos/juegoBusquedaPares/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Letras",
+    descripcion: "",
+    imagen: "/miniProyectos/letras/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/letras/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Loading",
+    descripcion: "",
+    imagen: "/miniProyectos/loading/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/loading/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Mosaico Círculos",
+    descripcion: "",
+    imagen: "/miniProyectos/mosaicoCirculos/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/mosaicoCirculos/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Netflix",
+    descripcion: "",
+    imagen: "/miniProyectos/netflix/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/netflix/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Orbitas",
+    descripcion: "",
+    imagen: "/miniProyectos/orbitas/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/orbitas/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Pájaros",
+    descripcion: "",
+    imagen: "/miniProyectos/pajaro/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/pajaro/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Animación Patrones de Líneas",
+    descripcion: "",
+    imagen: "/miniProyectos/patronesLineas/fondo.png",
+    tag: "animaciones", // Etiqueta para filtrar
+    enlace: "/miniProyectos/patronesLineas/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Piano - Juego",
+    descripcion: "",
+    imagen: "/miniProyectos/piano/fondo.png",
+    tag: "juegos", // Etiqueta para filtrar
+    enlace: "/miniProyectos/piano/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "San Valentín",
+    descripcion: "",
+    imagen: "/miniProyectos/sanValentin/fondo.png",
+    tag: "juegos", // Etiqueta para filtrar
+    enlace: "/miniProyectos/sanValentin/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Serpiente - Juego",
+    descripcion: "",
+    imagen: "/miniProyectos/serpiente/fondo.png",
+    tag: "juegos", // Etiqueta para filtrar
+    enlace: "/miniProyectos/serpiente/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "Super Héroes",
+    descripcion: "",
+    imagen: "/miniProyectos/super heroes/fondo.png",
+    tag: "otros", // Etiqueta para filtrar
+    enlace: "/miniProyectos/super heroes/index.html", // Enlace a la página del proyecto
+  },
+  {
+    titulo: "To do List",
+    descripcion: "",
+    imagen: "/miniProyectos/toDoList/fondo.png",
+    tag: "otros", // Etiqueta para filtrar
+    enlace: "/miniProyectos/toDoList/index.html", // Enlace a la página del proyecto
+  },
   // Agrega más cartas según sea necesario
 ];
-
 // Función para renderizar cartas
 function renderCartas(filterTag = "todos") {
   const contenedorCartas = document.getElementById("cartas");
